11ab64e5fa08b74e9fc4a6b69f34feb1f1164e61
gunmeet <gunmeets@gmail.com>
2025-08-30 18:10:16 +0530

    Update Product class to log addition results and adjust weather forecast range

--- Product/Program.cs
+++ Product/Program.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Product.Add(4, 5);
 
 
 public class Product
 {
     public static int Add(int a, int b)
     {
+        Console.WriteLine(a + b);
         return a + b;
     }
 }
